b9f439814503d817e3d398b4c9d7d179b85c3f60
antonio sands <moogle.x10@gmail.com>
2024-03-22 12:54:30 +0000

    DQM3 Event - Part 3

---
+++ abilitytable.js
@@ -0,0 +1,32 @@
+$(document).ready( function () {
+    $('#myTable').DataTable({
+        columnDefs: [
+            { orderable: false, targets: 0}
+            ,{ orderable: false, targets: 3}
+            ,{ orderable: false, targets: 4}
+            ,{ visible: false, targets: 7}
+            ,{ visible: false, targets: 8}
+            ,{ visible: false, targets: 9}
+            ,{ visible: false, targets: 10}
+        ],
+        order: [[2, 'asc']],
+        "pageLength": 25
+      });
+
+    $('input:radio[name=rank]').change(function() {
+        $('#myTable').DataTable().columns(7).search(this.value).draw();
+    });
+    
+    $('input:radio[name=colour]').change(function() {
+      $('#myTable').DataTable().columns(8).search(this.value).draw();
+    });
+
+    $('input:radio[name=type]').change(function() {
+        $('#myTable').DataTable().columns(9).search(this.value).draw();
+      });
+
+    $('input:radio[name=element]').change(function() {
+        $('#myTable').DataTable().columns(10).search(this.value).draw();
+    });
+
+} );
---
+++ unitscripts/kinggodwyn.js
@@ -0,0 +1,47 @@
+var Awk = 0;
+var AwkStatBuff =   {"5":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
+                    ,"4":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
+                    ,"3":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
+                    ,"2":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
+                    ,"1":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
+                    ,"0":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}};
+
+var AwkStatsUp =    {"5":{"code":null,"hp":1.25,"mp":1.25,"atk":1.25,"def":1.25,"agl":1.25,"wis":1.25,"move":1,"stype":null},"4":{"code":null,"hp":1.2,"mp":1.2,"atk":1.2,"def":1.2,"agl":1.2,"wis":1.2,"move":1,"stype":null},"3":{"code":null,"hp":1.15,"mp":1.15,"atk":1.15,"def":1.15,"agl":1.15,"wis":1.15,"move":1,"stype":null},"2":{"code":null,"hp":1.1,"mp":1.1,"atk":1.1,"def":1.1,"agl":1.1,"wis":1.1,"move":1,"stype":null},"1":{"code":null,"hp":1.05,"mp":1.05,"atk":1.05,"def":1.05,"agl":1.05,"wis":1.05,"move":1,"stype":null},"0":{"code":null,"hp":1,"mp":1,"atk":1,"def":1,"agl":1,"wis":1,"move":1,"stype":null}}; 
+
+var TalentBlossomStats = {"code":null,"hp":150,"mp":30,"atk":15,"def":25,"agl":15,"wis":15,"move":0,"stype":null};
+var FullTalent = false;
+
+function applyStats() {
+    $('#HP').text(Math.floor((1024 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].hp : 0) + (FullTalent ? TalentBlossomStats.hp : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].hp : 0)));
+    $('#MP').text(Math.floor((406 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].mp : 0) + (FullTalent ? TalentBlossomStats.mp : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].mp : 0)));
+    $('#ATK').text(Math.floor((300 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].atk : 0) + (FullTalent ? TalentBlossomStats.atk : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].atk : 0)));
+    $('#DEF').text(Math.floor((244 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].def : 0) + (FullTalent ? TalentBlossomStats.def : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].def : 0)));
+    $('#AGL').text(Math.floor((432 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].agl : 0) + (FullTalent ? TalentBlossomStats.agl : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].agl : 0)));
+    $('#WIS').text(Math.floor((239 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].wis : 0) + (FullTalent ? TalentBlossomStats.wis : 0)) * (AwkStatsUp[Awk] ? AwkStatsUp[Awk].wis : 0)));
+    $('#Move').text(2 + ((AwkStatBuff[Awk] ? AwkStatBuff[Awk].move : 0) + (FullTalent ? TalentBlossomStats.move : 0)));
+}
+
+$(document).ready( function () {
+applyStats();
+$(".formawk").on("change", function() {
+    Awk = $(this).val();
+    applyStats();
+});
+if ($("#TalentBlossom").is(":checked")) {
+    FullTalent = true;
+    applyStats();
+} else {
+    FullTalent = false;
+    applyStats();
+}
+$("#TalentBlossom").change(function() {
+    if ($(this).is(":checked")) {
+        FullTalent = true;
+        applyStats();
+    } else {
+        FullTalent = false;
+        applyStats();
+    }
+});
+
+} );
