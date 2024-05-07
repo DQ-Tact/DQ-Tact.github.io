aefa576346f9bbbbaab191253d6a57d230a169ea
antonio sands <moogle.x10@gmail.com>
2024-05-07 11:51:04 +0100

    True DQ VII Part 2 & Sortable Family/Rank Columns

--- datatable.js
+++ datatable.js
@@ -1,39 +1,35 @@
 $(document).ready( function () {
     $('#myTable').DataTable({
         columnDefs: [
-          { orderable: false, targets: 1}
-          ,{ orderable: false, targets: 3}
-          ,{ orderable: false, targets: 4}
+          { orderable: false, targets: 1}  
           ,{ visible: false, targets: 13}
           ,{ visible: false, targets: 14}
-          ,{ visible: false, targets: 15}
-          ,{ visible: false, targets: 16}
         ],
         order: [[0, 'asc']],
         "pageLength": 25
       });
 
     $('input:radio[name=rank]').change(function() {
-        $('#myTable').DataTable().columns(13).search(this.value).draw();
+        $('#myTable').DataTable().columns(3).search(this.value).draw();
     });
     
     $('input:radio[name=family]').change(function() {
-      $('#myTable').DataTable().columns(14).search(this.value).draw();
+      $('#myTable').DataTable().columns(4).search(this.value).draw();
     });
 
     $('#TalentBlossom').change(function() {
       if ($(this).is(':checked')) {
-        $('#myTable').DataTable().columns(15).search("1").draw();
+        $('#myTable').DataTable().columns(13).search("1").draw();
       } else {
-        $('#myTable').DataTable().columns(15).search("").draw();
+        $('#myTable').DataTable().columns(13).search("").draw();
       }
     });
     
     $('#CharBuilder').change(function() {
       if ($(this).is(':checked')) {
-        $('#myTable').DataTable().columns(16).search("1").draw();
+        $('#myTable').DataTable().columns(14).search("1").draw();
       } else {
-        $('#myTable').DataTable().columns(16).search("").draw();
+        $('#myTable').DataTable().columns(14).search("").draw();
       }
     });
 } );
