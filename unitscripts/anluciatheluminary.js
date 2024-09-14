d3da17c05e84ee25845db58ee67dd3e5ba3526de
antonio sands <moogle.x10@gmail.com>
2024-09-14 16:20:49 +0100

    DQ X Jewel Event part 1 & DQ X Translations & Base Game S Blossoms

--- unitscripts/anluciatheluminary.js
+++ unitscripts/anluciatheluminary.js
@@ -1,4 +1,5 @@
 var Awk = 0;
+var Elemental = 0;
 var AwkStatBuff =   {"5":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
                     ,"4":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
                     ,"3":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}
@@ -11,12 +12,13 @@ var AwkStatsUp =    {"5":{"code":null,"hp":1.25,"mp":1.25,"atk":1.25,"def":1.25,
 var TalentBlossomStats = {"code":null,"hp":100,"mp":30,"atk":15,"def":25,"agl":15,"wis":15,"move":0,"stype":null};
 var TalentBlossomStatsUpNode = {"code":null,"hp":0.05,"mp":0.05,"atk":0.05,"def":0.05,"agl":0.05,"wis":0.05,"move":0,"stype":null};
 var FullTalent = false;
+var ElementalStatsUp =    {"9":{"code":null,"hp":0.07,"mp":0.05,"atk":0,"def":0.05,"agl":0,"wis":0,"move":0,"stype":null},"8":{"code":null,"hp":0.04,"mp":0.04,"atk":0,"def":0.04,"agl":0,"wis":0,"move":0,"stype":null},"7":{"code":null,"hp":0.04,"mp":0.03,"atk":0,"def":0.03,"agl":0,"wis":0,"move":0,"stype":null},"6":{"code":null,"hp":0.03,"mp":0.03,"atk":0,"def":0.03,"agl":0,"wis":0,"move":0,"stype":null},"5":{"code":null,"hp":0.03,"mp":0.02,"atk":0,"def":0.02,"agl":0,"wis":0,"move":0,"stype":null},"4":{"code":null,"hp":0.02,"mp":0.02,"atk":0,"def":0.02,"agl":0,"wis":0,"move":0,"stype":null},"3":{"code":null,"hp":0.02,"mp":0.01,"atk":0,"def":0.01,"agl":0,"wis":0,"move":0,"stype":null},"2":{"code":null,"hp":0.01,"mp":0.01,"atk":0,"def":0.01,"agl":0,"wis":0,"move":0,"stype":null},"1":{"code":null,"hp":0.01,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null},"0":{"code":null,"hp":0,"mp":0,"atk":0,"def":0,"agl":0,"wis":0,"move":0,"stype":null}}; 
 
 function applyStats() {
-    $('#HP').text(Math.floor((946 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].hp : 0) + (FullTalent ? TalentBlossomStats.hp : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].hp : 0) + (FullTalent ? TalentBlossomStatsUpNode.hp : 0))));
-    $('#MP').text(Math.floor((315 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].mp : 0) + (FullTalent ? TalentBlossomStats.mp : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].mp : 0) + (FullTalent ? TalentBlossomStatsUpNode.mp : 0))));
+    $('#HP').text(Math.floor((946 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].hp : 0) + (FullTalent ? TalentBlossomStats.hp : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].hp : 0) + (FullTalent ? TalentBlossomStatsUpNode.hp : 0) + (ElementalStatsUp[Elemental] ? ElementalStatsUp[Elemental].hp : 0))));
+    $('#MP').text(Math.floor((315 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].mp : 0) + (FullTalent ? TalentBlossomStats.mp : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].mp : 0) + (FullTalent ? TalentBlossomStatsUpNode.mp : 0) + (ElementalStatsUp[Elemental] ? ElementalStatsUp[Elemental].mp : 0))));
     $('#ATK').text(Math.floor((486 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].atk : 0) + (FullTalent ? TalentBlossomStats.atk : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].atk : 0) + (FullTalent ? TalentBlossomStatsUpNode.atk : 0))));
-    $('#DEF').text(Math.floor((313 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].def : 0) + (FullTalent ? TalentBlossomStats.def : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].def : 0) + (FullTalent ? TalentBlossomStatsUpNode.def : 0))));
+    $('#DEF').text(Math.floor((313 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].def : 0) + (FullTalent ? TalentBlossomStats.def : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].def : 0) + (FullTalent ? TalentBlossomStatsUpNode.def : 0) + (ElementalStatsUp[Elemental] ? ElementalStatsUp[Elemental].def : 0))));
     $('#AGL').text(Math.floor((381 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].agl : 0) + (FullTalent ? TalentBlossomStats.agl : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].agl : 0) + (FullTalent ? TalentBlossomStatsUpNode.agl : 0))));
     $('#WIS').text(Math.floor((392 + (AwkStatBuff[Awk] ? AwkStatBuff[Awk].wis : 0) + (FullTalent ? TalentBlossomStats.wis : 0)) * ((AwkStatsUp[Awk] ? AwkStatsUp[Awk].wis : 0) + (FullTalent ? TalentBlossomStatsUpNode.wis : 0))));
     $('#Move').text(3 + ((AwkStatBuff[Awk] ? AwkStatBuff[Awk].move : 0) + (FullTalent ? TalentBlossomStats.move : 0)));
@@ -26,6 +28,10 @@ $(document).ready( function () {
 applyStats();
 $(".formawk").on("change", function() {
     Awk = $(this).val();
+    applyStats();
+});
+$(".formelement").on("change", function() {
+    Elemental = $(this).val();
     applyStats();
 });
 if ($("#TalentBlossom").is(":checked")) {
